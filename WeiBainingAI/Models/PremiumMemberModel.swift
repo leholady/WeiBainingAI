d85363a12f7a179bf3ea8d4d7ee371c785995453
yy2021_8689 <yuluo--@outlook.com>
2023-12-04 18:29:17 +0800

    PremiumMemberModel upload

--- WeiBainingAI/Models/PremiumMemberModel.swift
+++ WeiBainingAI/Models/PremiumMemberModel.swift
@@ -6,6 +6,11 @@
 //
 
 import Foundation
+import StoreKit
+
+struct PremiumMemberResultModel: Codable {
+    var datas: [PremiumMemberModel]
+}
 
 struct PremiumMemberModel: Codable, Equatable, Identifiable, Hashable {
     var id: String {
@@ -13,5 +18,35 @@ struct PremiumMemberModel: Codable, Equatable, Identifiable, Hashable {
     }
     
     var productId: String
-    var title: String
+    var state: PremiumMemberPageModel.PremiumMemberPageState
+    
+    var title: String?
+    var duration: Int?
+    var unit: String?
+    
+    var price: String?
+    var code: String?
+    var symbol: String?
+    
+    func newModelTo(_ product: Product) -> PremiumMemberModel {
+        var model = self
+        switch model.state {
+        case .quota:
+            model.title = product.displayName.isEmpty ? "\(model.duration ?? 0)\(model.unit ?? "")" : product.displayName
+        default:
+            model.title = product.displayName.isEmpty ? model.title : product.displayName
+        }
+        model.price = product.displayPrice
+        model.code = product.format.currencyCode
+        model.symbol = product.format.currencySymbol
+        return model
+    }
+}
+
+extension Product {
+    var format: NumberFormatter {
+        let format = NumberFormatter()
+        format.locale = priceFormatStyle.locale
+        return format
+    }
 }
