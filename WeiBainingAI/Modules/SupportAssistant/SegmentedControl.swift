9292935e14843741aad7a333a48a91adbe58b488
yy2021_8689 <yuluo--@outlook.com>
2023-12-07 16:08:20 +0800

    SegmentedControl update

--- WeiBainingAI/Modules/SupportAssistant/SegmentedControl.swift
+++ WeiBainingAI/Modules/SupportAssistant/SegmentedControl.swift
@@ -26,28 +26,70 @@ struct SegmentedControl: View {
     @Namespace var namespace
     
     var body: some View {
-        HStack(alignment: .center, spacing: 0) {
-            ForEach(0..<items.count, id: \.self) { index in
-                Rectangle()
-                    .foregroundColor(.white.opacity(0.01))
-                    .overlay(content: {
-                        Text(items[index])
-                            .font(configuratiion.textFont)
-                            .foregroundColor(selectedIndex == index ? configuratiion.textSelectColor : configuratiion.textColor)
-                            .minimumScaleFactor(0.5)
-                            .lineLimit(1)
-                            .padding(.horizontal, 5)
-                    })
-                    .matchedGeometryEffect(
-                        id: index,
-                        in: namespace,
-                        isSource: true
-                    )
-                    .onTapGesture {
-                        withAnimation {
-                            selectedIndex = index
+        ZStack {
+            if items.count > 5 {
+                ScrollViewReader { proxy in
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(alignment: .center, spacing: 0) {
+                            ForEach(0..<items.count, id: \.self) { index in
+                                Rectangle()
+                                    .id(index)
+                                    .frame(width: 84)
+                                    .foregroundColor(.white.opacity(0.01))
+                                    .overlay(content: {
+                                        Text(items[index])
+                                            .font(configuratiion.textFont)
+                                            .foregroundColor(selectedIndex == index ? configuratiion.textSelectColor : configuratiion.textColor)
+                                            .minimumScaleFactor(0.5)
+                                            .lineLimit(1)
+                                            .padding(.horizontal, 5)
+                                    })
+                                    .matchedGeometryEffect(
+                                        id: index,
+                                        in: namespace,
+                                        isSource: true
+                                    )
+                                    .onTapGesture {
+                                        withAnimation {
+                                            selectedIndex = index
+                                            if #available(iOS 16.0, *) {
+                                                proxy.scrollTo(selectedIndex, anchor: .center)
+                                            } else {
+                                                proxy.scrollTo(selectedIndex, anchor: .trailing)
+                                            }
+                                        }
+                                    }
+                            }
                         }
+                        .frame(height: configuratiion.height)
                     }
+                    .animation(.default, value: selectedIndex)
+                }
+            } else {
+                HStack(alignment: .center, spacing: 0) {
+                    ForEach(0..<items.count, id: \.self) { index in
+                        Rectangle()
+                            .foregroundColor(.white.opacity(0.01))
+                            .overlay(content: {
+                                Text(items[index])
+                                    .font(configuratiion.textFont)
+                                    .foregroundColor(selectedIndex == index ? configuratiion.textSelectColor : configuratiion.textColor)
+                                    .minimumScaleFactor(0.5)
+                                    .lineLimit(1)
+                                    .padding(.horizontal, 5)
+                            })
+                            .matchedGeometryEffect(
+                                id: index,
+                                in: namespace,
+                                isSource: true
+                            )
+                            .onTapGesture {
+                                withAnimation {
+                                    selectedIndex = index
+                                }
+                            }
+                    }
+                }
             }
         }
         .background {
