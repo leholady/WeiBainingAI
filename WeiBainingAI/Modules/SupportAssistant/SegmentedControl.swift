509bc684e25959eebbd2ece087499f3677fab299
yy2021_8689 <yuluo--@outlook.com>
2023-12-08 14:50:11 +0800

    SegmentedControl remove  ZStack

--- WeiBainingAI/Modules/SupportAssistant/SegmentedControl.swift
+++ WeiBainingAI/Modules/SupportAssistant/SegmentedControl.swift
@@ -26,85 +26,96 @@ struct SegmentedControl: View {
     @Namespace var namespace
     
     var body: some View {
-        ZStack {
-            if items.count > 5 {
-                ScrollViewReader { proxy in
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(alignment: .center, spacing: 0) {
-                            ForEach(0..<items.count, id: \.self) { index in
-                                Rectangle()
-                                    .id(index)
-                                    .frame(width: 84)
-                                    .foregroundColor(.white.opacity(0.01))
-                                    .overlay(content: {
-                                        Text(items[index])
-                                            .font(configuratiion.textFont)
-                                            .foregroundColor(selectedIndex == index ? configuratiion.textSelectColor : configuratiion.textColor)
-                                            .minimumScaleFactor(0.5)
-                                            .lineLimit(1)
-                                            .padding(.horizontal, 5)
-                                    })
-                                    .matchedGeometryEffect(
-                                        id: index,
-                                        in: namespace,
-                                        isSource: true
-                                    )
-                                    .onTapGesture {
-                                        withAnimation {
-                                            selectedIndex = index
-                                            if #available(iOS 16.0, *) {
-                                                proxy.scrollTo(selectedIndex, anchor: .center)
-                                            } else {
-                                                proxy.scrollTo(selectedIndex, anchor: .trailing)
-                                            }
+        if items.count > 5 {
+            ScrollViewReader { proxy in
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(alignment: .center, spacing: 0) {
+                        ForEach(0..<items.count, id: \.self) { index in
+                            Rectangle()
+                                .id(index)
+                                .frame(width: 84)
+                                .foregroundColor(.white.opacity(0.01))
+                                .overlay(content: {
+                                    Text(items[index])
+                                        .font(configuratiion.textFont)
+                                        .foregroundColor(selectedIndex == index ? configuratiion.textSelectColor : configuratiion.textColor)
+                                        .minimumScaleFactor(0.5)
+                                        .lineLimit(1)
+                                        .padding(.horizontal, 5)
+                                })
+                                .matchedGeometryEffect(
+                                    id: index,
+                                    in: namespace,
+                                    isSource: true
+                                )
+                                .onTapGesture {
+                                    withAnimation {
+                                        selectedIndex = index
+                                        if #available(iOS 16.0, *) {
+                                            proxy.scrollTo(selectedIndex, anchor: .center)
+                                        } else {
+                                            proxy.scrollTo(selectedIndex, anchor: .trailing)
                                         }
                                     }
+                                }
+                        }
+                    }
+                    .frame(height: configuratiion.height - 4)
+                }
+                .background {
+                    Rectangle()
+                        .fill(configuratiion.selectColor)
+                        .cornerRadius(configuratiion.selectRadius)
+                        .matchedGeometryEffect(
+                            id: selectedIndex,
+                            in: namespace,
+                            isSource: false
+                        )
+                }
+                .padding(2)
+                .frame(height: configuratiion.height)
+                .background(configuratiion.backgroundColor)
+                .cornerRadius(configuratiion.backgroundRadius)
+            }
+        } else {
+            HStack(alignment: .center, spacing: 0) {
+                ForEach(0..<items.count, id: \.self) { index in
+                    Rectangle()
+                        .foregroundColor(.white.opacity(0.01))
+                        .overlay(content: {
+                            Text(items[index])
+                                .font(configuratiion.textFont)
+                                .foregroundColor(selectedIndex == index ? configuratiion.textSelectColor : configuratiion.textColor)
+                                .minimumScaleFactor(0.5)
+                                .lineLimit(1)
+                                .padding(.horizontal, 5)
+                        })
+                        .matchedGeometryEffect(
+                            id: index,
+                            in: namespace,
+                            isSource: true
+                        )
+                        .onTapGesture {
+                            withAnimation {
+                                selectedIndex = index
                             }
                         }
-                        .frame(height: configuratiion.height)
-                    }
-                    .animation(.default, value: selectedIndex)
-                }
-            } else {
-                HStack(alignment: .center, spacing: 0) {
-                    ForEach(0..<items.count, id: \.self) { index in
-                        Rectangle()
-                            .foregroundColor(.white.opacity(0.01))
-                            .overlay(content: {
-                                Text(items[index])
-                                    .font(configuratiion.textFont)
-                                    .foregroundColor(selectedIndex == index ? configuratiion.textSelectColor : configuratiion.textColor)
-                                    .minimumScaleFactor(0.5)
-                                    .lineLimit(1)
-                                    .padding(.horizontal, 5)
-                            })
-                            .matchedGeometryEffect(
-                                id: index,
-                                in: namespace,
-                                isSource: true
-                            )
-                            .onTapGesture {
-                                withAnimation {
-                                    selectedIndex = index
-                                }
-                            }
-                    }
                 }
             }
+            .background {
+                Rectangle()
+                    .fill(configuratiion.selectColor)
+                    .cornerRadius(configuratiion.selectRadius)
+                    .matchedGeometryEffect(
+                        id: selectedIndex,
+                        in: namespace,
+                        isSource: false
+                    )
+            }
+            .padding(2)
+            .frame(height: configuratiion.height)
+            .background(configuratiion.backgroundColor)
+            .cornerRadius(configuratiion.backgroundRadius)
         }
-        .background {
-            Rectangle()
-                .fill(configuratiion.selectColor)
-                .cornerRadius(configuratiion.selectRadius)
-                .matchedGeometryEffect(
-                    id: selectedIndex,
-                    in: namespace,
-                    isSource: false
-                )
-        }
-        .padding(2)
-        .frame(height: configuratiion.height)
-        .background(configuratiion.backgroundColor)
-        .cornerRadius(configuratiion.backgroundRadius)
     }
 }
